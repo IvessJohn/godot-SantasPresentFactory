75e195026086d9c6a5a541defc61ae95a5af7b8b
IvessJohn <vanek2v@gmail.com>
2021-12-13 17:11:48 +0200

    Implemented WanderingNPCSM

--- source/actors/WanderingNPC.gd
+++ source/actors/WanderingNPC.gd
@@ -1,3 +1,29 @@
 extends Actor
 class_name WanderingNPC
 
+
+# Min and max duration of staying idle
+export(Dictionary) var idle_time = {"min": 2.0, "max": 10.0}
+# Min and max duration of wandering
+export(Dictionary) var wander_time = {"min": 2.0, "max": 10.0}
+
+var wander_direction: Vector2 = Vector2.ZERO
+
+onready var wanderSM = $WanderingNPCStateMachine
+onready var wanderTimer = $WanderTimer
+onready var idleTimer = $IdleTimer
+onready var wanderRayCast = $WanderRayCast2D
+
+
+func should_wander() -> bool:
+	return idleTimer.is_stopped()
+
+func should_idle() -> bool:
+	return (wanderTimer.is_stopped() or wanderRayCast.is_colliding())
+
+
+func get_wanderTimer_new_duration() -> float:
+	return rand_range(wander_time["min"], wander_time["max"])
+
+func get_idleTimer_new_duration() -> float:
+	return rand_range(idle_time["min"], idle_time["max"])
--- source/actors/WanderingNPCStateMachine.gd
+++ source/actors/WanderingNPCStateMachine.gd
@@ -4,15 +4,42 @@ func _ready():
 	add_state("idle")
 	add_state("move")
 	call_deferred("set_state", states.idle)
+#	call_deferred("set_state", states.move)
 
 func _state_logic(delta):
-	pass
+	match state:
+		states.idle:
+			parent.velocity = Vector2.ZERO
+		states.move:
+			parent.velocity = parent.SPEED * parent.wander_direction
+	
+	parent._move()
 
-func get_transition(delta):
-	pass
+func _get_transition(delta):
+	match state:
+		states.idle:
+			if parent.should_wander():
+				return states.move
+		states.move:
+			if parent.should_idle():
+				return states.idle
 
 func _exit_state(old_state, new_state):
 	pass
 
 func _enter_state(new_state, old_state):
-	pass
+	match new_state:
+		states.idle:
+			parent.idleTimer.start(parent.get_idleTimer_new_duration())
+		states.move:
+			parent.wanderTimer.start(parent.get_wanderTimer_new_duration())
+			
+			var deg = rand_range(0, 360)
+			parent.wanderRayCast.rotation_degrees = deg
+			var attempts := 1
+			while parent.wanderRayCast.is_colliding() or attempts <= 10:
+				deg = rand_range(0, 360)
+				parent.wanderRayCast.rotation_degrees = deg
+				attempts += 1
+			
+			parent.wander_direction = Vector2.RIGHT.rotated(deg2rad(deg))
