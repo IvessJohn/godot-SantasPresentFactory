0fa1c50b5a8184a5ae8009f2fa86f79dcd636280
IvessJohn <vanek2v@gmail.com>
2021-12-14 06:41:07 +0200

    Added PlaceableObject 'type' property

--- source/resources/PlaceableObjects/PlaceableObjectResource.gd
+++ source/resources/PlaceableObjects/PlaceableObjectResource.gd
@@ -1,7 +1,14 @@
 extends Resource
 class_name PlaceableObjectResource
 
+enum OBJECT_TYPES {
+	PROP,
+	ACTOR,
+	BUILDING
+}
+
 export(String) var name: String
+export(OBJECT_TYPES) var type := OBJECT_TYPES.PROP
 export(Texture) var ui_icon: Texture
 export(Texture) var texture: Texture
 export(PackedScene) var scene: PackedScene
