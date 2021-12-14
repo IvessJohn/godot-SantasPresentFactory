69b1bdf8fec59fbb220bfa074a017eeeb0b4ef3e
IvessJohn <vanek2v@gmail.com>
2021-12-14 17:33:15 +0200

    Void attempts to implement AnimationTree

--- source/actors/ActorBase.gd
+++ source/actors/ActorBase.gd
@@ -5,7 +5,11 @@ export(int) var SPEED := 100
 
 var velocity := Vector2.ZERO
 
+onready var sprite = $Sprite
+onready var collShape = $CollisionShape2D
+onready var animPlayer = $AnimationPlayer
 onready var animTree = $AnimationTree
+onready var animState = animTree.get("parameters/playback")
 
 
 func _move():
--- source/actors/WanderingNPC.gd
+++ source/actors/WanderingNPC.gd
@@ -2,6 +2,11 @@ extends Actor
 class_name WanderingNPC
 
 
+export(Dictionary) var TEXTURES := {
+	"idle": null,
+	"move": null,
+}
+
 # Min and max duration of staying idle
 export(Dictionary) var idle_time = {"min": 2.0, "max": 10.0}
 # Min and max duration of wandering
@@ -15,16 +20,15 @@ onready var idleTimer = $IdleTimer
 onready var wanderRayCast = $WanderRayCast2D
 
 
+func _ready():
+	randomize()
+	pass
+
 func should_wander() -> bool:
 	return idleTimer.is_stopped()
 
 func should_idle() -> bool:
 	return (wanderTimer.is_stopped() or wanderRayCast.is_colliding())
-
-func _physics_process(delta):
-	#Update AnimationTree for showing proper animations
-	animTree.set("parameters/Idle/blend_position", wander_direction)
-	animTree.set("parameters/Move/blend_position", wander_direction)
 
 func get_wanderTimer_new_duration() -> float:
 	return rand_range(wander_time["min"], wander_time["max"])
