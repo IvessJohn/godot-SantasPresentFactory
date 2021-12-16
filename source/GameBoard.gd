7115193264300e171ed610ebcf25054fc6415537
IvessJohn <vanek2v@gmail.com>
2021-12-16 06:27:33 +0200

    Removed PropIDResource class
    1. Removed the rebundant PropIDResource class
    2. Removed unnecessary comments in GameBoard.gd

--- source/GameBoard.gd
+++ source/GameBoard.gd
@@ -4,7 +4,6 @@ class_name GameBoard
 const DIRECTIONS = [Vector2.LEFT, Vector2.UP, Vector2.RIGHT, Vector2.DOWN]
 
 export(Resource) var grid: Resource = preload("res://source/resources/Grid.tres")
-export(Resource) var props_res: Resource  = preload("res://source/resources/PropIDResource.tres")
 
 # Each key-value pair represents an object. The key is the position, the value is the reference to
 # the object
@@ -72,11 +71,7 @@ func set_selected_resource(value):
 # OBEJCT PLACEMENT
 #
 func place_object(cell: Vector2):
-	# Place tile
-	# tilemap.set_cellv(...)
-	# tilemap.update_bitmask_region()
-#	var object_scene: PackedScene = props_res.get_prop(prop_id)
-	print(can_place_object(cell, _selected_resource))
+#	print(can_place_object(cell, _selected_resource))
 	if can_place_object(cell, _selected_resource):
 		match _selected_resource.type:
 			PlaceableObjectResource.OBJECT_TYPES.PROP:
@@ -112,7 +107,7 @@ func _place_prop(cell, _prop_resource: PlaceableObjectResource):
 	var object_instance: Node2D = object_scene.instance()
 	objects.add_child(object_instance)
 	
-#				print((cell))
+#	print((cell))
 	var object_pos: Vector2 = grid.calculate_map_position(cell)
 	object_instance.global_position = object_pos
 	
@@ -124,13 +119,12 @@ func _place_actor(cell, _actor_resource: PlaceableObjectResource):
 	var actor_instance: Node2D = actor_scene.instance()
 	objects.add_child(actor_instance)
 	
-#				print((cell))
+#	print((cell))
 	var object_pos: Vector2 = grid.calculate_map_position(cell)
 	actor_instance.global_position = object_pos
 	
 	_actors.append(actor_instance)
 #	_objects[cell] = object_instance
-#	_props[cell] = object_instance
 
 func _place_tile(cell, _tile_resource: PlaceableTileResource):
 	var tilemap: TileMap = tilemaps[_tile_resource.tile_placement]
