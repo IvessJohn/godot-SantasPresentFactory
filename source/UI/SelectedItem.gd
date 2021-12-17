f6bd51e47e0078fbf23c4a8b4198d19f8a6fb30b
IvessJohn <vanek2v@gmail.com>
2021-12-17 06:11:28 +0200

    SelectedItem shows the object type

--- source/UI/SelectedItem.gd
+++ source/UI/SelectedItem.gd
@@ -4,6 +4,7 @@ var _selected_resource: PlaceableObjectResource
 
 onready var itemIcon = $VBoxContainer/ItemIcon
 onready var itemLabel = $VBoxContainer/ItemLabel
+onready var typeLabel = $VBoxContainer/TypeLabel
 
 
 func _update_selected_object(value):
@@ -11,7 +12,24 @@ func _update_selected_object(value):
 		_selected_resource = value
 		itemIcon.texture = _selected_resource.ui_icon
 		itemLabel.text = _selected_resource.name
+		typeLabel.text = _get_object_type_in_text(_selected_resource.type)
 
 
 func _on_TileVerticalList_tile_chosen(tile_resource):
 	_update_selected_object(tile_resource)
+
+
+func _get_object_type_in_text(object_type: int):
+	match object_type:
+		PlaceableObjectResource.OBJECT_TYPES.PROP:
+			return "Prop"
+		PlaceableObjectResource.OBJECT_TYPES.TILE:
+			return "Tile"
+		PlaceableObjectResource.OBJECT_TYPES.ACTOR:
+			return "NPC"
+		PlaceableObjectResource.OBJECT_TYPES.DECORATION:
+			return "Decoration"
+		PlaceableObjectResource.OBJECT_TYPES.BUILDING:
+			return "Building"
+		_:
+			return "Undefined"
