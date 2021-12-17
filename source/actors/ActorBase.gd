411b0aff86e3661562887d3c463d5afa1d8710d5
IvessJohn <vanek2v@gmail.com>
2021-12-17 06:40:28 +0200

    Switch to GLES2 + unsuccessful Actor's InteractionaArea implementation

--- source/actors/ActorBase.gd
+++ source/actors/ActorBase.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 class_name Actor
 
+signal clicked
+
 export(int) var SPEED := 100
 
 var velocity := Vector2.ZERO
@@ -14,3 +16,12 @@ onready var animState = animTree.get("parameters/playback")
 
 func _move():
 	velocity = move_and_slide(velocity)
+
+
+func _on_InteractionArea_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			emit_signal("clicked")
+
+func _on_clicked():
+	print(name + " was just clicked!")
